6b86339db6115d1eaac0f401c8172da34610b5be
UsmanYap <107064481+UsmanYap@users.noreply.github.com>
2022-12-20 21:07:16 +0700

    menambahkan design baru

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,38 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-            <nav className="navbar navbar-expand-lg bg-light shadow-lg">
-                <div className="container">
-                    <div className="navbar-brand">
-                        <img src={`${process.env.PUBLIC_URL}/assets/images/logo.png`} className="logo img-fluid" alt="Kind Heart Charity"/>
-                        <span>
-                            Kind Heart Charity
-                            <small>Non-profit Organization</small>
-                        </span>
-                    </div>
+  return (
+    <nav className="navbar navbar-expand-lg bg-light shadow-lg">
+      <div className="container">
+        <div className="navbar-brand">
+          <img src={`${process.env.PUBLIC_URL}/assets/images/logo.img`} className="logo img-fluid" alt="Kind Heart Charity" />
+          <span>Kind Heart Charity</span>
+        </div>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link click-scroll">Home</Link>
-                            </li>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link click-scroll">
+                Home
+              </Link>
+            </li>
 
-                            <li className="nav-item">
-                                <Link to="/charity" className="nav-link click-scroll">Charity</Link>
-                            </li>
+            <li className="nav-item">
+              <Link to="/charity" className="nav-link click-scroll">
+                Charity
+              </Link>
+            </li>
 
-                            <li className="nav-item">
-                                <Link to="/mycharity" className="nav-link click-scroll">My Charity</Link>
-                            </li>
+            <li className="nav-item">
+              <Link to="/mycharity" className="nav-link click-scroll">
+                My Charity
+              </Link>
+            </li>
 
-                            {/* <li className="nav-item dropdown">
+            {/* <li className="nav-item dropdown">
                                 <div className="nav-link click-scroll dropdown-toggle"
                                     id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown"
                                     aria-expanded="false">My Charity</div>
@@ -44,18 +46,20 @@ const Navbar = () => {
                                 </ul>
                             </li> */}
 
-                            <li className="nav-item">
-                                <div className="nav-link click-scroll" >Contact</div>
-                            </li>
+            <li className="nav-item">
+              <div className="nav-link click-scroll">Contact</div>
+            </li>
 
-                            <li className="nav-item ms-3">
-                                <Link to="Login" className="nav-link custom-btn custom-border-btn btn">Login</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-    )
-}
+            <li className="nav-item ms-3">
+              <Link to="Login" className="nav-link custom-btn custom-border-btn btn">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
